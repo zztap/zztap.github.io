f2f6fdceeaf1b0dc1254e16b670a0c16ac6eb724
zztap <coolxtx@gmail.com>
2025-06-03 21:00:35 +0800

    Site updated: 2025-06-03 21:00:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/01/hello-world/"];function toRandomPost(){
+var posts=["2025/06/03/这是一篇新的博文/","2025/06/01/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
