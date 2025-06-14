2bc7d30550f1e3e5cd50c7b1a981b837b7ca29da
zztap <zztap@users.noreply.github.com>
2025-06-14 00:48:43 +0000

    deploy: zztap/zztap.github.io@4666236de55580e7c30a5aa59b09e8a8eb3ee6fd

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/13/hello-world/","2025/06/13/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2025/06/14/hello-world/","2025/06/13/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
